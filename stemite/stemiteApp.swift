a48a7dc3fe7a5e82a4fa3026240b662d9d5ab295
Ruben <ruben.suarezdz@udlap.mx>
2025-05-12 19:04:10 -0600

    Initial Commit

---
+++ stemite/stemiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  stemiteApp.swift
+//  stemite
+//
+//  Created by iOS Lab on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct stemiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ stemiteTests/stemiteTests.swift
@@ -0,0 +1,17 @@
+//
+//  stemiteTests.swift
+//  stemiteTests
+//
+//  Created by iOS Lab on 12/05/25.
+//
+
+import Testing
+@testable import stemite
+
+struct stemiteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
